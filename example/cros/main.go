da2dc022c8645d73e48ffbb0ba69c63d0001bd23
pzrr@qq.com <pzrr@qq.com>
2017-06-08 20:03:43 +0800

    * 新增cros中间件，参考 github.com/devfeel/middleware/cros * 支持较为丰富的option选项，可以根据实际业务场景自定义 * 支持设置Header： * 1)Access-Control-Allow-Origin * 2)Access-Control-Allow-Methods * 3)Access-Control-Allow-Headers * 4)Access-Control-Allow-Credentials * 5)Access-Control-Expose-Headers * 6)Access-Control-Max-Age * 7)P3P * Demo: github.com/devfeel/middleware/example/cros * 2017-06-08 20:00

---
+++ example/cros/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/devfeel/dotweb"
+	"github.com/devfeel/middleware/cros"
+	"strconv"
+)
+
+const JwtContextKey = "jwtuser"
+
+func main() {
+	//初始化DotServer
+	app := dotweb.New()
+
+	//设置dotserver日志目录
+	//如果不设置，默认不启用，且默认为当前目录
+	app.SetEnabledLog(true)
+
+	//开启development模式
+	app.SetDevelopmentMode()
+
+	//设置路由
+	InitRoute(app.HttpServer)
+
+	//设置HttpModule
+	//InitModule(app)
+
+	//启动 监控服务
+	//app.SetPProfConfig(true, 8081)
+
+	// 开始服务
+	port := 8080
+	fmt.Println("dotweb.StartServer => " + strconv.Itoa(port))
+	err := app.StartServer(port)
+	fmt.Println("dotweb.StartServer error => ", err)
+}
+
+func Index(ctx dotweb.Context) error {
+	_, err := ctx.WriteString("Index: ", dotweb.HeaderAccessControlAllowMethods, " => ", ctx.Response().QueryHeader(dotweb.HeaderAccessControlAllowMethods))
+	return err
+}
+
+func Simple(ctx dotweb.Context) error {
+	_, err := ctx.WriteString("Simple: ", dotweb.HeaderAccessControlAllowMethods, " => ", ctx.Response().QueryHeader(dotweb.HeaderAccessControlAllowMethods))
+	return err
+}
+
+func NoCros(ctx dotweb.Context) error {
+	_, err := ctx.WriteString("NoCros: ", dotweb.HeaderAccessControlAllowMethods, " => ", ctx.Response().QueryHeader(dotweb.HeaderAccessControlAllowMethods))
+	return err
+}
+
+func InitRoute(server *dotweb.HttpServer) {
+	server.Router().GET("/", Index).Use(NewCustomCROS())
+	server.GET("/simple", Simple).Use(NewSimpleCROS())
+	server.Router().GET("/nocros", NoCros)
+}
+
+// NewSimpleCROS
+// default config:
+// enabledCROS = true
+// allowedOrigins = "*"
+// allowedMethods = "GET, POST, PUT, DELETE, OPTIONS"
+// allowedHeaders = "Content-Type"
+// allowedP3P = "CP=\"CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR\""
+func NewSimpleCROS() dotweb.Middleware {
+	option := cros.NewConfig().UseDefault()
+	return cros.NewMiddleware(option)
+}
+
+func NewCustomCROS() dotweb.Middleware {
+	option := cros.NewConfig()
+	option.SetHeader("Content-Type")
+	option.SetMethod("GET, POST")
+	option.Enabled()
+	return cros.NewMiddleware(option)
+}
