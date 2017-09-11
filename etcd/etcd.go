690c70516be7e31555ca58de1e5c6d229c8f5f89
Sunsuke <sunsuke@SunsukedeMacBook-Pro.local>
2017-09-11 13:20:04 +0800

    优化初始化流程, 实现目录和节点的update

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -1,14 +1,13 @@
 package etcd
 
 import (
-	//etcd "github.com/coreos/go-etcd/etcd"
 	"github.com/devfeel/dotweb"
 	"io/ioutil"
 	"os"
 	"gopkg.in/yaml.v2"
-	"path/filepath"
 	"fmt"
 	"github.com/coreos/go-etcd/etcd"
+	"strings"
 )
 const RootPath = "/dotweb"	//etcd的根路径
 
@@ -21,9 +20,7 @@ type  EtcdMiddleware struct{
 	Config *Config
 	Client *etcd.Client
 }
-func init(){
 
-}
 
 func (m *EtcdMiddleware) Handle(ctx dotweb.Context) error {
 	fmt.Println("handle function of etcd middleware")
@@ -31,17 +28,21 @@ func (m *EtcdMiddleware) Handle(ctx dotweb.Context) error {
 }
 
 /**
-	list root path dirs of etcd
+	return root node of etcd
  */
-func (m *EtcdMiddleware) List() (etcd.Nodes, error) {
+func (m *EtcdMiddleware) RootNode() (*etcd.Node, error) {
 	response,err := m.Client.Get(RootPath,false,true)
+
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("%+v \n",*response.Node)
-	return response.Node.Nodes, nil
+
+	return response.Node, nil
 }
 
+/**
+	add a new dir
+ */
 func (m *EtcdMiddleware) AddNewDirectory(path string) error {
 	_,err := m.Client.CreateDir(RootPath + path,0)
 	if err != nil{
@@ -50,6 +51,9 @@ func (m *EtcdMiddleware) AddNewDirectory(path string) error {
 	return nil
 }
 
+/**
+	add a new node
+ */
 func (m *EtcdMiddleware) AddNewNode(key string, value string, ttl uint64) error{
 	_, err := m.Client.Create(RootPath + key, value, ttl)
 	if err != nil{
@@ -74,10 +78,31 @@ func (m *EtcdMiddleware) RemoveDir(key string) error{
 	return nil
 }
 
-// Middleware new create a AccessLog Middleware
+func (m *EtcdMiddleware) UpdateDir(key string) error{
+	_, err := m.Client.UpdateDir(RootPath + key, 0)
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (m *EtcdMiddleware) UpdateNode(key string) error{
+	_, err := m.Client.UpdateDir(RootPath + key, 0)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
+
+// Middleware new create a Etcd Middleware
 func Middleware(path string) *EtcdMiddleware {
 	conf := InitConfig(path)		//default path is ./etcd_conf.yaml
 	client := InitClientConnect(conf)
+	InitDirtory(client)
+
 	return &EtcdMiddleware{Config:conf,Client:client}
 }
 
@@ -101,14 +126,12 @@ func InitConfig(configFilePath string) *Config {
 		panic("read config file of etcd middleware error\n" +
 			"path: " + configFilePath + "\n" +
 			err.Error())
-		os.Exit(1)
 	}
 	err = yaml.Unmarshal(content, &config)
 	if err != nil {
 		panic("parse config file of etcd middleware error\n" +
 			"path: " + configFilePath + "\n" +
 			err.Error())
-		os.Exit(1)
 	}
 	return &config
 }
@@ -120,40 +143,19 @@ func InitClientConnect(conf *Config)(client *etcd.Client){
 
 func InitDirtory(client *etcd.Client){
 	if _,err := client.Get(RootPath,false,true);err != nil{
-
+		if getEtcdErrorCode(err.Error()) == "100"{
+			client.CreateDir(RootPath,0)
+		}else {
+			panic(err.Error())
+		}
 	}
 
 }
 
-
-func lookupFile(configFile string) (realFile string, exists bool) {
-	//add default file lookup
-	//1、按绝对路径检查
-	//2、尝试在当前进程根目录下寻找
-	//3、尝试在当前进程根目录/config/ 下寻找
-	//fixed for (#3 当使用json配置的时候，运行会抛出panic)
-	realFile = configFile
-	exists = true
-	if !fileExists(realFile) {
-		realFile = getCurrentDirectory() + "/" + configFile
-		exists = false
-	}
-	if !exists && !fileExists(realFile) {
-		realFile = getCurrentDirectory() + "/config/" + configFile
-	} else {
-		exists = true
-	}
-	if !exists && fileExists(realFile) {
-		exists = true
-	}
-	return realFile, exists
-}
-
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil || os.IsExist(err)
-}
-
-func getCurrentDirectory() string {
-	return filepath.Clean(filepath.Dir(os.Args[0])) + "/"
+/**
+	返回etcd 客户端 错误码
+ */
+func getEtcdErrorCode(errmsg string) string{
+	errs := strings.Split(errmsg,":")
+	return errs[0]
 }
